acffca86ad873632de1566db4b170b6a5691034c
meraeugene <andrewvillalon123@gmail.com>
2023-08-13 20:33:51 +0800

    833

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,23 +6,20 @@ import Recommend from "./pages/Recommend";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import { SkeletonTheme } from "react-loading-skeleton";
-import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
     <SkeletonTheme baseColor="#313131" highlightColor="#525252">
-      <AnimatePresence>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/films" element={<Films />}></Route>
-            <Route path="/recommend" element={<Recommend />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </AnimatePresence>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/films" element={<Films />}></Route>
+          <Route path="/recommend" element={<Recommend />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </SkeletonTheme>
   );
 }
