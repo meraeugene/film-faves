bd3123504527db29b1c6871acc57c14e2e0850c9
meraeugene <andrewvillalon123@gmail.com>
2023-08-16 11:28:09 +0800

    11

--- backend/server.js
+++ backend/server.js
@@ -14,15 +14,7 @@ app.use((req, res, next) => {
 });
 
 // Enable CORS for all routes
-app.use(
-  cors({
-    allowedHeaders: ["sessionId", "Content-Type"],
-    exposedHeaders: ["sessionId"],
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-  })
-);
+app.use(cors());
 
 //to get the req.body
 app.use(express.json());
