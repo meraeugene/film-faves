2077c2e2a83fecf82c887a56d4c07e60c14b1b19
meraeugene <andrewvillalon123@gmail.com>
2023-08-20 15:28:38 +0800

    328

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,24 +5,21 @@ import Films from "./pages/Films";
 import Recommend from "./pages/Recommend";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import { SkeletonTheme } from "react-loading-skeleton";
 import FilmDetails from "./pages/FilmDetails";
 
 function App() {
   return (
-    <SkeletonTheme baseColor="#313131" highlightColor="#525252">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/films" element={<Films />}></Route>
-          <Route path="/films/:id" element={<FilmDetails />}></Route>
-          <Route path="/recommend" element={<Recommend />}></Route>
-          <Route path="/auth/signup" element={<Signup />}></Route>
-          <Route path="/auth/login" element={<Login />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </SkeletonTheme>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/films" element={<Films />}></Route>
+        <Route path="/films/:id" element={<FilmDetails />}></Route>
+        <Route path="/recommend" element={<Recommend />}></Route>
+        <Route path="/auth/signup" element={<Signup />}></Route>
+        <Route path="/auth/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
